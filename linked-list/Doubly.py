c6cb4e36dca3e18ccab80cd6b6a62a38ad462edd
cyxabima <ukashaanwerali@gmail.com>
2025-08-03 10:22:52 +0500

    linked-list

---
+++ linked-list/Doubly.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, val):
+        self.prev = None
+        self.val = val
+        self.next = None
+
+
+class Doubly:
+    def __init__(self):
+        self.head = None
